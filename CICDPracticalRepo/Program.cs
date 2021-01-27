5d7160bde3e8eaaabe0d87a9a6e1e0a5595196c6
NihilPatel <nihil@promactinfo.com>
2021-01-27 16:12:21 +0530

    changes in webconfig

--- CICDPracticalRepo/Program.cs
+++ CICDPracticalRepo/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using System.IO;
 
 namespace CICDPracticalRepo
 {
@@ -13,7 +14,14 @@ namespace CICDPracticalRepo
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = new WebHostBuilder()
+             .UseKestrel()
+             .UseContentRoot(Directory.GetCurrentDirectory())
+             .UseIISIntegration()
+             .UseStartup<Startup>()
+             .Build();
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
